d6877c8b148591e8fd52ecfad26905b8874bb5e7
NI04Code <naufalichsan114@gmail.com>
2024-02-12 20:34:23 +0700

    add jacoco test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
 }
@@ -61,6 +62,19 @@ tasks.register<Test>("functionalTest") {
         includeTestsMatching("*FunctionalTest")
     }
 }
+
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+tasks.test {
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
 }
